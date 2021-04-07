6b62a3f1f255fc56a530c942ee67ed8db35246f4
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-04-08 00:12:53 +0300

    start home work

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+import Searchbar from "../Searchbar";
+import ImageGallery from "../ImageGallery";
+import Loader from "../Loader";
+import Button from "../Button";
+import Modal from "../Modal";
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,1 @@
+import scss from "./Searchbar.module.scss";
